3c3b8bc37909adfd43aa199d45e020412cff8699
xank <xank@qq.com>
2018-06-09 19:07:50 +0800

    card

--- Card/Card.vue
+++ Card/Card.vue
@@ -1,9 +1,25 @@
 <style lang='stylus'>
+@require '../../../styles/disney/var/color.styl';
+
 .dm-card {
+  &__header {
+    margin-bottom: 20px;
+    padding-bottom: 30px;
+    color: $color-grey;
+    border-bottom: 1px solid $color-light-grey-ss;
+
+    span {
+      font-size: 17px;
+      font-weight: 600;
+    }
+  }
 }
 </style>
 <template>
   <div class="dm-card">
+    <div class="dm-card__header">
+      <slot name="header"></slot>
+    </div>
     <slot></slot>
   </div>
 </template>
